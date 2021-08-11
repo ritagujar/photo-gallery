c9ff98dc3022e71cd89b23b75972046097a3edee
Rita-Gujar <ritagujar14@gmail.com>
2021-08-11 23:16:01 +0530

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>PhotoGallery</h1>
+      <h2>Your Pictures</h2>
+      <p>Click on the bellow "+" sign and choose the photo you wanna save...</p>
+    </div>
+  );
+};
+
+export default Title;
