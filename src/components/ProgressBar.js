0f6502345a2b99e8493dbf6a7c89149cb9fb0457
Rita-Gujar <ritagujar14@gmail.com>
2021-08-16 22:53:15 +0530

    makking & styling a progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
-const ProgressBar = () => {
-  return <div className="progress-bar">progress</div>;
+const ProgressBar = ({ file, setFile }) => {
+  const { url, progress } = useStorage(file);
+
+  // To remove the progress bar once image is uploaded
+  // Once we have the url it will takes the bar off
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import { projectStorage } from "../firebase/config";
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from "../firebase/config";
 
 const useStorage = (file) => {
   const [progress, setProgrees] = useState(0);
@@ -9,6 +13,7 @@ const useStorage = (file) => {
   useEffect(() => {
     // Creating a reference if file into the default firebase storage.
     const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection("images");
 
     // Uploading the file to the ubove reference
     storageRef.put(file).on(
@@ -23,6 +28,8 @@ const useStorage = (file) => {
       },
       async () => {
         const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        collectionRef.add({ url, createdAt });
         setUrl(url);
       }
     );
